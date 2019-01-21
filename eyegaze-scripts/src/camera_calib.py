f2d64f6f71977595d39f3bda64b2ad3431d06319
EmersonBoyd <emerson.boyd@yahoo.com>
2019-01-21 10:59:22 -0500

    Update the calibration script to perform image flattening

--- eyegaze-scripts/src/camera_calib.py
+++ eyegaze-scripts/src/camera_calib.py
@@ -2,6 +2,9 @@ import numpy as np
 import cv2 as cv
 import glob
 
+# following this tutorial:
+# https://docs.opencv.org/4.0.0/dc/dbb/tutorial_py_calibration.html
+
 # these numbers indicated the number of interior corners
 num_rows = 8
 num_cols = 6
@@ -16,7 +19,7 @@ objp[:,:2] = np.mgrid[0:num_rows, 0:num_cols].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
-images = glob.glob('../res/pycam_calib_twisttie/*.jpg')
+images = glob.glob('../res/picam_calib_twisttie/*.jpg')
 
 for fname in images:
     img = cv.imread(fname)
@@ -29,13 +32,62 @@ for fname in images:
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+
+        corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners)
+
         # Draw and display the corners
         cv.drawChessboardCorners(img, (num_rows, num_cols), corners2, ret)
-        cv.imshow('img', img)
-        cv.waitKey(500)
+        cv.imshow(fname, img)
+        cv.waitKey(5000)
+
     else:
-        print('failed to find corners for', fname)
+        print('Failed to find corners for', fname)
+        exit(1)
 
 cv.destroyAllWindows()
+
+
+
+
+print('Calculating camera calibration matrix...')
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+
+
+
+print('Undistorting the picam photos...')
+for fname in images:
+    img = cv.imread(fname)
+    h, w = img.shape[:2]
+    newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+    print(newcameramtx)
+    print(roi)
+
+    # undistort
+    dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+   
+    # crop the image based on valid pixels calculated from getOptimalNewCameraMatrix()
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+    cv.imwrite('calibresult.png', dst)
+
+    # # Draw the image
+    # cv.imshow('img'. )
+
+
+
+
+print('Calculating reprojection error...')
+mean_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    mean_error += error
+print( "total error: {}".format(mean_error/len(objpoints)) )
+
+
+
+
+print ('Done...')
