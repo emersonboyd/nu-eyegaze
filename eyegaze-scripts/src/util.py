8836562c58fec76f52c8300f2d27a261b7a5600d
EmersonBoyd <emerson.boyd@yahoo.com>
2019-02-08 11:21:17 -0500

    Save camera calibration values using pickle so that photos can be undistorted much faster later

--- eyegaze-scripts/src/util.py
+++ eyegaze-scripts/src/util.py
@@ -1,5 +1,10 @@
 import os
 
+
+def file_exists(path):
+	return os.path.exists(path)
+
+
 def get_resources_directory():
 	this_directory = os.path.dirname(os.path.abspath(__file__))
 	return '{}/../res'.format(this_directory)
