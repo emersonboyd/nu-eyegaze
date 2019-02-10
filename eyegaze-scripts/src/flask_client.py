ed817275e3c19031b53896d29b87abab00e9121a
Brendon Welsh <welsh.b@husky.neu.edu>
2019-02-09 19:44:04 -0500

    modified test webserver and client, working now

--- eyegaze-scripts/src/flask_client.py
+++ eyegaze-scripts/src/flask_client.py
@@ -1,8 +1,10 @@
-
+import numpy as np
+import json
 import requests
+import random
 import time
-
 while True:
-    request = requests.post("http://35.138.63.125:5000/hello")
+
+    request = requests.post("http://35.185.63.125:5000/hello", data='data')
     time.sleep(.1)
     print(request.text)
