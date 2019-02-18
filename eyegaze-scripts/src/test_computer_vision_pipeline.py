bc465184a5f231076faedb201274ec268e0826c1
EmersonBoyd <emerson.boyd@yahoo.com>
2019-02-18 10:43:51 -0500

    Rename a test file to be more descriptive

--- eyegaze-scripts/src/test_computer_vision_pipeline.py
+++ eyegaze-scripts/src/test_computer_vision_pipeline.py
@@ -3,8 +3,11 @@ import cv2 as cv
 from constants import CameraType
 import util
 import image_helper as ih
+import time
 
 def run():
+    start = time.time()
+
     camera_type = CameraType.EMERSON_IPHONE_6_PLUS
     test_path = '{}/iphone_6_plus_emerson_calib_test'.format(util.get_resources_directory())
     image_left = cv.imread('{}/IMG_5736.JPG'.format(test_path), cv.IMREAD_GRAYSCALE)
@@ -23,10 +26,11 @@ def run():
         exit(1)
     depth = ih.calculate_depth(pixel_left, est_pixel_right, camera_type)
 
-    ih.show_image_with_mark(image_left, pixel_left)
+    # ih.show_image_with_mark(image_left, pixel_left)
 
     print('estimated depth in left image pixel ({}, {}): {} meters'.format(pixel_left[0], pixel_left[1], depth))
 
+    print(time.time() - start)
 
 if __name__ == '__main__':
     run()
