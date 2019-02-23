970cf6944b789b93de8bad5f2e6a87c62dc5e767
brendon <welsh.b@husky.neu.edu>
2019-02-23 12:57:49 -0500

    get push button working with flask webserver

--- eyegaze-scripts/src/flask_client.py
+++ eyegaze-scripts/src/flask_client.py
@@ -4,8 +4,16 @@ import requests
 import random
 import time
 
-img = open('../res/test_photos/20190108_091908.jpg', 'rb')
-files = {'media': img}
-request = requests.post("http://35.185.63.125:5000/inf", files=files)
-time.sleep(.1)
-print(request.text)
+def send_image(image_name, ip="35.185.63.125", port="5000", command="inf"):
+    """
+    Send an image to the Google Cloud Server
+
+    """
+
+    img = open(image_name, 'rb')
+    post_command = "http://" + ip + ":" + port + "/" + command
+    print(post_command)
+    files = {'media': img}
+    request = requests.post(post_command, files=files)
+    time.sleep(.1)
+    print(request.text)
