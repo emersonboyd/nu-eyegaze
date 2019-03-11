4e09c0e86c451eb8a38136970244b97b0c7b0861
Emerson Boyd <emerson.boyd@yahoo.com>
2019-03-11 12:14:46 -0400

    Add the newly trained model to the repo

--- eyegaze-scripts/src/tf_export_graph.sh
+++ eyegaze-scripts/src/tf_export_graph.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source ~/.bashrc
 python3 --version
-python3 export_inference_graph.py --input_type image_tensor --pipeline_config_path=../res/sign_labels/ssd_mobilenet_v1_pets.config --trained_checkpoint_prefix ../res/sign_training/model.ckpt-7715 --output_directory ../res/sign_model
+python3 export_inference_graph.py --input_type image_tensor --pipeline_config_path=../res/sign_labels/ssd_mobilenet_v1_pets.config --trained_checkpoint_prefix ../res/sign_training/model.ckpt-56728 --output_directory ../res/sign_model
