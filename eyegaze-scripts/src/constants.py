cafb4a8b64470bfe095a39cd7891938e44dd0812
EmersonBoyd <emerson.boyd@yahoo.com>
2019-03-12 15:04:01 -0400

    Separte the classifcation code into functions for the server to use

--- eyegaze-scripts/src/constants.py
+++ eyegaze-scripts/src/constants.py
@@ -6,3 +6,24 @@ class CameraType(Enum):
     PICAM_RIGHT = 2
     EMERSON_IPHONE_6_PLUS = 3
 
+
+class ClassType(Enum):
+    EXIT_SIGN = 'exit_sign', 1
+    BATHROOM_SIGN = 'bathroom_sign', 2
+
+    def __str__(self):
+        return self.value[0]
+
+    def num(self):
+        return self.value[1]
+
+
+def get_class_type_for_number(n):
+    for c in ClassType:
+        if c.value[1] == n:
+            return c
+
+
+if __name__ == '__main__':
+    e = ClassType.EXIT_SIGN
+    print(e)
--- eyegaze-scripts/src/label_image.py
+++ eyegaze-scripts/src/label_image.py
@@ -24,6 +24,13 @@ import tensorflow as tf
 from os.path import exists
 from google.protobuf import text_format
 from tensorflow.python.platform import gfile
+import util
+
+from PIL import Image
+from include.models.research.object_detection.utils import label_map_util
+
+import constants
+
 
 
 def load_graph(model_file):
@@ -147,3 +154,115 @@ def label_image(file_name):
     return_string = return_string + result + " "
 
   return return_string
+
+
+#
+#
+# INITIALIZE MODEL AND VARIABLES UPON IMPORT OF THIS SCRIPT
+#
+#
+MINIMUM_CONFIDENCE = 0.98
+MODEL_NAME = '{}/sign_model'.format(util.get_resources_directory())
+PATH_TO_FROZEN_GRAPH = '{}/frozen_inference_graph.pb'.format(MODEL_NAME)
+PATH_TO_LABELS = '{}/sign_labels/object-detection.pbtxt'.format(util.get_resources_directory())
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+#
+#
+#
+#
+#
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+
+def run_inference_for_single_image(image, graph):
+  with graph.as_default():
+    with tf.Session() as sess:
+      # Get handles to input and output tensors
+      ops = tf.get_default_graph().get_operations()
+      all_tensor_names = {output.name for op in ops for output in op.outputs}
+      tensor_dict = {}
+      for key in [
+          'num_detections', 'detection_boxes', 'detection_scores',
+          'detection_classes', 'detection_masks'
+      ]:
+        tensor_name = key + ':0'
+        if tensor_name in all_tensor_names:
+          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+              tensor_name)
+      if 'detection_masks' in tensor_dict:
+        # The following processing is only for single image
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[0], image.shape[1])
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        # Follow the convention by adding back the batch dimension
+        tensor_dict['detection_masks'] = tf.expand_dims(
+            detection_masks_reframed, 0)
+      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+      # Run inference
+      output_dict = sess.run(tensor_dict,
+                             feed_dict={image_tensor: np.expand_dims(image, 0)})
+
+      # all outputs are float32 numpy arrays, so convert types as appropriate
+      output_dict['num_detections'] = int(output_dict['num_detections'][0])
+      output_dict['detection_classes'] = output_dict[
+          'detection_classes'][0].astype(np.uint8)
+      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+      output_dict['detection_scores'] = output_dict['detection_scores'][0]
+      if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+  return output_dict
+
+
+def get_classification_dict_for_image(image_path):
+  image = Image.open(image_path)
+  image_np = load_image_into_numpy_array(image)
+  image_np_expanded = np.expand_dims(image_np, axis=0)
+  output_dict = run_inference_for_single_image(image_np, detection_graph)
+
+  return output_dict
+
+
+def get_string_for_classification_dict(d):
+  confidneces = d['detection_scores']
+  result_string = ''
+
+  for i, confidnece in enumerate(confidneces):
+    if confidnece < MINIMUM_CONFIDENCE:
+      break
+
+    class_enum = constants.get_class_type_for_number(d['detection_classes'][i])
+    result_string += '{} 15.2 35.2 '.format(str(class_enum))
+
+  return result_string
+
+
+def get_response_string_with_image_path(image_path):
+  classification_dict = get_classification_dict_for_image(image_path)
+  response_string = get_string_for_classification_dict(classification_dict)
+  return response_string
+
+
+if __name__ == '__main__':
+  image_path = '{}/sign_photos/test/image111.jpg'.format(util.get_resources_directory())
+  print(get_response_string_with_image_path(image_path))
