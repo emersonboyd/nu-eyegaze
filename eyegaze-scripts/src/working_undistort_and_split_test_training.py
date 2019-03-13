bfa4f7f757fba72f4e2f2f581f3a72eb86441e07
EmersonBoyd <emerson.boyd@yahoo.com>
2019-03-13 15:19:19 -0400

    Swap the left and the right images for pickle matrices

--- eyegaze-scripts/src/working_undistort_and_split_test_training.py
+++ eyegaze-scripts/src/working_undistort_and_split_test_training.py
@@ -9,7 +9,7 @@ import util
 
 
 PERCENTAGE_TEST_IMAGES = 0.07
-CAMERA_TYPE = CameraType.PICAM_RIGHT
+CAMERA_TYPE = CameraType.PICAM_LEFT
 
 HOME_PATH = expanduser("~")
 ORIGINAL_IMAGE_PATH = glob.glob('{}/Desktop/PiCam Signs/*.jpg'.format(HOME_PATH))
@@ -17,6 +17,9 @@ UNDISTORTED_IMAGE_PATH_TRAIN = '{}/sign_photos/trainTHISPROTECTSAGAINSTOVERWRITI
 UNDISTORTED_IMAGE_PATH_TEST = '{}/sign_photos/testTHISPROTECTSAGAINSTOVERWRITING'.format(util.get_resources_directory())
 
 mtx, dist = ih.get_calib_data_for_camera_type(CAMERA_TYPE)
+
+test_images = ['image15.jpg', 'image35.jpg', 'image37.jpg', 'image41.jpg', 'image59.jpg', 'image63.jpg', 'image77.jpg', 'image99.jpg', 'image103.jpg', 'image111.jpg', 'image121.jpg', 'image145.jpg', 'image151.jpg', 'image171.jpg', 'image176.jpg']
+print(len(test_images))
 
 i = 1
 for original_image_path in ORIGINAL_IMAGE_PATH:
@@ -26,7 +29,7 @@ for original_image_path in ORIGINAL_IMAGE_PATH:
     undistorted_image_name = 'image{}.jpg'.format(i)
 
     # check whether we should save the photo as a test photo or a train photo
-    if random.uniform(0, 1) > PERCENTAGE_TEST_IMAGES:
+    if undistorted_image_name not in test_images:
         undistorted_image_path = '{}/{}'.format(UNDISTORTED_IMAGE_PATH_TRAIN, undistorted_image_name)
     else:
         undistorted_image_path = '{}/{}'.format(UNDISTORTED_IMAGE_PATH_TEST, undistorted_image_name)
